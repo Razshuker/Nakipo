bc2090aa020aab64719f93f2d3888401fa6322c8
Razshuker <raz.shuker13@gmail.com>
2025-05-06 21:21:25 +0300

    add report location

--- Nakipo/Models/WalletReport.cs
+++ Nakipo/Models/WalletReport.cs
@@ -7,13 +7,20 @@ public class WalletReport
     [BsonElement("image")]
 
     public string Image { get; set; }
+
     [BsonElement("date")]
 
     public DateTime Date { get; set; }
     [BsonElement("expiration-date")]
 
     public DateTime ExpirationDate { get; set; } 
+   
     [BsonElement("report-used")]
 
     public bool ReportUsed { get; set; }
+    [BsonElement("location")]
+
+    public Location Location { get; set; }
+    
+    
 }
