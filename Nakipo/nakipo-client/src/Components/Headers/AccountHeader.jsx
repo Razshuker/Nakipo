f690d33347b9f3939c5a2cdcd7f8c8d62dae2388
Razshuker <raz.shuker13@gmail.com>
2025-05-08 10:25:40 +0300

    change folder location

---
+++ Nakipo/nakipo-client/src/Components/Headers/AccountHeader.jsx
@@ -0,0 +1,88 @@
+import Header from "./Header";
+import '../../CSS/header.css';
+import { useLocation, useNavigate } from "react-router-dom";
+import {useGetUserQuery} from "../Auth/AuthApiSlice";
+
+export default function AccountHeader() {
+    const nav = useNavigate();
+    const location = useLocation();
+    let pathname = location.search;
+    const { data:user, isLoading } = useGetUserQuery();
+
+    function changeTab(tabLink) {
+        nav(tabLink);
+    }
+
+    return (
+        <header>
+            <Header />
+            <div className="user-info dark-blue text-dark-coral py-4 text-center">
+                {isLoading ? <h2>Loading...</h2> :
+                <h2>היי {user && user.username}</h2>
+                }
+            </div>
+
+            <div className="bottom-bar row row-cols-2">
+                <div className="col wallet p-0">
+                    <div
+                        onClick={() => changeTab("/?wallet")}
+                        className={`${(pathname === "?wallet" || pathname === "") ? "text-dark-coral denim" : "dark-coral text-dark-blue"} mt-1 me-1 mb-1 py-2 px-3`}
+                    >
+                        <img
+                            src={(pathname !== "?wallet" || pathname !== "") ? "/files/5_1_blue.png" : "/files/5_1_off.png"}
+                            height={15}
+                            className="col-auto p-0"
+                            alt="wallet"
+                        />
+                        <p className="col-auto">ארנק</p>
+                    </div>
+                </div>
+
+                <div className="col rating-table p-0">
+                    <div
+                        onClick={() => changeTab("/?rating")}
+                        className={`${pathname === "?rating" ? "text-dark-coral denim" : "dark-coral text-dark-blue"} mt-1 ms-1 me-1 mb-1 py-2 px-3`}
+                    >
+                        <img
+                            src={pathname !== "?rating" ? "/files/5_2_blue.png" : "/files/5_2_off.png"}
+                            height={15}
+                            className="col-auto p-0"
+                            alt="rating"
+                        />
+                        <p className="col-auto">טבלה</p>
+                    </div>
+                </div>
+
+                <div className="col challange p-0">
+                    <div
+                        onClick={() => changeTab("/?challange")}
+                        className={`${pathname === "?challange" ? "text-dark-coral denim" : "dark-coral text-dark-blue"} me-1 mb-1 py-2 px-3`}
+                    >
+                        <img
+                            src={pathname !== "?challange" ? "/files/5_3_blue.png" : "/files/5_3_off.png"}
+                            height={15}
+                            className="col-auto p-0"
+                            alt="challange"
+                        />
+                        <p className="col-auto">אתגר</p>
+                    </div>
+                </div>
+
+                <div className="col feedo p-0">
+                    <div
+                        onClick={() => changeTab("/?feedo")}
+                        className={`${pathname === "?feedo" ? "text-dark-coral denim" : "dark-coral text-dark-blue"} ms-1 me-1 mb-1 py-2 px-3`}
+                    >
+                        <img
+                            src={pathname !== "?feedo" ? "/files/5_4_blue.png" : "/files/5_4_off.png"}
+                            height={15}
+                            className="col-auto p-0"
+                            alt="feedo"
+                        />
+                        <p className="col-auto">פידו</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
