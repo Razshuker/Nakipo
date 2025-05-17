8fc36a1f600106df1d61cf39b5a5ff8f0c301222
Razshuker <raz.shuker13@gmail.com>
2025-05-17 13:34:36 +0300

    remove unused code

--- Nakipo/nakipo-client/src/Components/Auth/Login.jsx
+++ Nakipo/nakipo-client/src/Components/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React from 'react'
 import {useForm} from 'react-hook-form'
 import {useGoogleLoginMutation, useLoginMutation} from "./AuthApiSlice";
 import {TextField} from "@mui/material";
