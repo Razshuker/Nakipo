a90a7d08e68e83407a2dbcc90d6e6b0c876cec01
Razshuker <raz.shuker13@gmail.com>
2025-05-25 15:06:13 +0300

    add logs (nLog)

--- Nakipo/Program.cs
+++ Nakipo/Program.cs
@@ -10,131 +10,145 @@ using Nakipo.Configurations;
 using Nakipo.Models;
 using Nakipo.Repositories;
 using Nakipo.Services;
+using NLog;
 using NLog.Web;
 
-var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
+logger.Debug("init");
+try
 {
-    Args = args,
-    WebRootPath = "wwwroot" 
-});
+    var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+    {
+        Args = args,
+        WebRootPath = "wwwroot"
+    });
 
-builder.Services.AddAuthentication(options =>
-    {
-        options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-        options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    })
-    .AddJwtBearer(options =>
-    {
-        options.TokenValidationParameters = new TokenValidationParameters()
+    builder.Services.AddAuthentication(options =>
         {
-            ValidateIssuer = false,
-            ValidateAudience = false,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ApplicationConfiguration.HashToken))
-        };
-    });
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(ApplicationConfiguration.HashToken))
+            };
+        });
 
-builder.Services.AddAuthentication(options =>
+    builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        })
+        .AddCookie()
+        .AddGoogle("Google", options =>
+        {
+            options.ClientId = ApplicationConfiguration.GoogleSettings.GoogleClientId;
+            options.ClientSecret = ApplicationConfiguration.GoogleSettings.GoogleClientSecret;
+        });
+
+    builder.Logging.ClearProviders();
+    builder.Host.UseNLog();
+
+    builder.Services.AddControllers().AddJsonOptions(options =>
     {
-        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-    })
-    .AddCookie()
-    .AddGoogle("Google", options =>
-    {
-        options.ClientId = ApplicationConfiguration.GoogleSettings.GoogleClientId;
-        options.ClientSecret = ApplicationConfiguration.GoogleSettings.GoogleClientSecret;
+        options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+        options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
     });
-
-
-builder.Host.UseNLog();
-
-builder.Services.AddControllers().AddJsonOptions(options =>
-{
-    options.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
-    options.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
-
-});
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+    {
         c.SwaggerDoc("v1", new OpenApiInfo() { Title = "dogood", Version = "v1" });
-   });
+    });
 
-builder.Services.AddHealthChecks()
-    .AddCheck("default", () => HealthCheckResult.Healthy());
+    builder.Services.AddHealthChecks()
+        .AddCheck("default", () => HealthCheckResult.Healthy());
 
 
 // builder.Services.AddMySqlDataSource(ApplicationConfiguration.DbSettings.MysqlConnectionString);
-builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection(nameof(MongoDBSettings)));
-builder.Services.AddSingleton<MongoDbContext>(_ => new MongoDbContext(ApplicationConfiguration.DbSettings.MongodbConnectionString, ApplicationConfiguration.DbSettings.MongodbDatabase));
-builder.Services.AddSingleton<IAuthService, AuthService>();
-builder.Services.AddSingleton<IUserRepository, UserRepository>();
-builder.Services.AddSingleton<IWalletService, WalletService>();
-builder.Services.AddSingleton<IWalletRepository, WalletRepository>();
-builder.Services.AddSingleton<IBrandService, BrandService>();
-builder.Services.AddSingleton<IBrandRepository, BrandRepository>();
-builder.Services.AddSingleton<IRatingService, RatingService>();
-builder.Services.AddSingleton<IImageService, ImageService>();
-builder.Services.AddHttpClient();
-builder.Services.AddSingleton<ISpaceService, SpaceService>();
-builder.Services.AddSingleton<IEmailService, EmailService>();
+    builder.Services.Configure<MongoDBSettings>(builder.Configuration.GetSection(nameof(MongoDBSettings)));
+    builder.Services.AddSingleton<MongoDbContext>(_ =>
+        new MongoDbContext(ApplicationConfiguration.DbSettings.MongodbConnectionString,
+            ApplicationConfiguration.DbSettings.MongodbDatabase));
+    builder.Services.AddSingleton<IAuthService, AuthService>();
+    builder.Services.AddSingleton<IUserRepository, UserRepository>();
+    builder.Services.AddSingleton<IWalletService, WalletService>();
+    builder.Services.AddSingleton<IWalletRepository, WalletRepository>();
+    builder.Services.AddSingleton<IBrandService, BrandService>();
+    builder.Services.AddSingleton<IBrandRepository, BrandRepository>();
+    builder.Services.AddSingleton<IRatingService, RatingService>();
+    builder.Services.AddSingleton<IImageService, ImageService>();
+    builder.Services.AddHttpClient();
+    builder.Services.AddSingleton<ISpaceService, SpaceService>();
+    builder.Services.AddSingleton<IEmailService, EmailService>();
 
 
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowReactApp", builder =>
+    builder.Services.AddCors(options =>
     {
-        builder.WithOrigins("http://localhost:3000")
-            .AllowCredentials()     
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .WithExposedHeaders("Authorization");
-    });
-});
-
-var app = builder.Build();
-
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-}
-
-app.UseDefaultFiles();
-
-app.UseStaticFiles(new StaticFileOptions
-{
-    OnPrepareResponse = ctx =>
-    {
-        // Disable caching in development
-        if (app.Environment.IsDevelopment())
+        options.AddPolicy("AllowReactApp", builder =>
         {
-            ctx.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
-        }
+            builder.WithOrigins("http://localhost:3000")
+                .AllowCredentials()
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithExposedHeaders("Authorization");
+        });
+    });
+
+    var app = builder.Build();
+
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseHsts();
     }
-});
-app.UseRouting();
-app.UseSwagger();
-app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nakipo"));
-app.Use(async (context, next) =>
+
+    app.UseDefaultFiles();
+
+    app.UseStaticFiles(new StaticFileOptions
+    {
+        OnPrepareResponse = ctx =>
+        {
+            // Disable caching in development
+            if (app.Environment.IsDevelopment())
+            {
+                ctx.Context.Response.Headers["Cache-Control"] = "no-cache, no-store";
+            }
+        }
+    });
+    app.UseRouting();
+    app.UseSwagger();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Nakipo"));
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers.Remove("Cross-Origin-Opener-Policy");
+        context.Response.Headers.Remove("Cross-Origin-Embedder-Policy");
+        context.Response.Headers["Cache-Control"] = "no-store, must-revalidate";
+        await next();
+    });
+    app.UseHttpsRedirection();
+    app.UseAuthentication();
+    app.UseAuthorization();
+    app.UseCors("AllowReactApp");
+    app.MapHealthChecks("/health");
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller}/{action=Index}/{id?}");
+
+    app.MapFallbackToFile("index.html");
+
+
+    app.Run();
+}
+catch (Exception e)
 {
-    context.Response.Headers.Remove("Cross-Origin-Opener-Policy");
-    context.Response.Headers.Remove("Cross-Origin-Embedder-Policy");
-    context.Response.Headers["Cache-Control"] = "no-store, must-revalidate";
-    await next();
-});
-app.UseHttpsRedirection();
-app.UseAuthentication();
-app.UseAuthorization();
-app.UseCors("AllowReactApp");
-app.MapHealthChecks("/health");
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller}/{action=Index}/{id?}");
-
-app.MapFallbackToFile("index.html");
-
-
-app.Run();
-
+    logger.Error(e, "Unhandled exception");
+}
+finally
+{
+    LogManager.Shutdown();
+}
