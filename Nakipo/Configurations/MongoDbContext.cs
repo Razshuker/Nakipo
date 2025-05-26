6756cda5da4716df6bb41bc74a3dd8efaa258640
Razshuker <raz.shuker13@gmail.com>
2025-05-26 11:00:14 +0300

    reorder code

--- Nakipo/Configurations/MongoDbContext.cs
+++ Nakipo/Configurations/MongoDbContext.cs
@@ -16,5 +16,7 @@ public class MongoDbContext
     public IMongoCollection<User> Users => _database.GetCollection<User>("users");
     public IMongoCollection<Cupon> Cupons => _database.GetCollection<Cupon>("cupons");
     public IMongoCollection<Brand> Brands => _database.GetCollection<Brand>("brands");
-    public IMongoCollection<ResetPasswordRequest> ResetPasswordRequests => _database.GetCollection<ResetPasswordRequest>("resetPasswordRequests");
+
+    public IMongoCollection<ResetPasswordRequest> ResetPasswordRequests =>
+        _database.GetCollection<ResetPasswordRequest>("resetPasswordRequests");
 }
